91971449121089e05f74a28a96e33e0f2fa873c4
tangmf <1040394043@qq.com>
2021-03-28 10:47:23 +0800

    二叉树结构修改

--- src/com/TreeNode.java
+++ src/com/TreeNode.java
@@ -6,39 +6,49 @@ package com;
  * @Description
  */
 public class TreeNode {
-	public int val;
-	public TreeNode left;
-	public TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
-	public TreeNode(int x) {
-		val = x;
-	}
 
-	// 前序遍历输出
-	public static void preOrderTraverse(TreeNode node) {
-		if (node == null)
-			return;
-		System.out.print(node.val + " ");
-		preOrderTraverse(node.left);
-		preOrderTraverse(node.right);
-	}
+    public TreeNode() {
+    }
 
-	// 中序遍历输出
-	public static void inOrderTraverse(TreeNode node) {
-		if (node == null)
-			return;
-		inOrderTraverse(node.left);
-		System.out.print(node.val + " ");
-		inOrderTraverse(node.right);
-	}
+    public TreeNode(int val) {
+        this.val = val;
+    }
 
-	// 后序遍历输出
-	public static void postOrderTraverse(TreeNode node) {
-		if (node == null)
-			return;
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    // 前序遍历输出
+    public static void preOrderTraverse(TreeNode node) {
+        if (node == null)
+            return;
+        System.out.print(node.val + " ");
+        preOrderTraverse(node.left);
+        preOrderTraverse(node.right);
+    }
+
+    // 中序遍历输出
+    public static void inOrderTraverse(TreeNode node) {
+        if (node == null)
+            return;
+        inOrderTraverse(node.left);
+        System.out.print(node.val + " ");
+        inOrderTraverse(node.right);
+    }
+
+    // 后序遍历输出
+    public static void postOrderTraverse(TreeNode node) {
+        if (node == null)
+            return;
         postOrderTraverse(node.left);
         postOrderTraverse(node.right);
-		System.out.print(node.val + " ");
-	}
+        System.out.print(node.val + " ");
+    }
 
 }
